fc937fe95acd107ad7706b7b550bcae6c5f5fa33
itstarfish <95844347+itstarfish@users.noreply.github.com>
2024-11-25 11:51:10 +0200

    a

--- Main.java
+++ Main.java
@@ -1,22 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        MyPoint p1 = new MyPoint();  // Test constructor
-        System.out.println(p1);      // Test toString()
-        p1.setX(8);   // Test setters
-        p1.setY(6);
-        System.out.println("x is: " + p1.getX());  // Test getters
-        System.out.println("y is: " + p1.getY());
-        p1.setXY(3, 0);   // Test setXY()
-        System.out.println(p1.getXY()[0]);  // Test getXY()
-        System.out.println(p1.getXY()[1]);
-        System.out.println(p1);
+        // Create MyPoint objects
+        MyPoint p1 = new MyPoint(0, 0);
+        MyPoint p2 = new MyPoint(3, 4);
 
-        MyPoint p2 = new MyPoint(0, 4);  // Test another constructor
-        System.out.println(p2);
-// Testing the overloaded methods distance()
-        System.out.println(p1.distance(p2));    // which version?
-        System.out.println(p2.distance(p1));    // which version?
-        System.out.println(p1.distance(5, 6));  // which version?
-        System.out.println(p1.distance());      // which version?
+        // Create MyLine objects using MyPoint objects
+        MyLine line1 = new MyLine(p1, p2);
+        System.out.println("Line1: " + line1);
+        System.out.println("Length of Line1: " + line1.getLength());
+        System.out.println("Gradient of Line1: " + line1.getGradient());
+
+        // Create MyLine objects using coordinates
+        MyLine line2 = new MyLine(1, 1, 4, 5);
+        System.out.println("Line2: " + line2);
+        System.out.println("Length of Line2: " + line2.getLength());
+        System.out.println("Gradient of Line2: " + line2.getGradient());
+
+        // Test setters and getters
+        line1.setBegin(new MyPoint(2, 2));
+        line1.setEnd(new MyPoint(5, 6));
+        System.out.println("Updated Line1: " + line1);
+        System.out.println("Updated Length of Line1: " + line1.getLength());
+        System.out.println("Updated Gradient of Line1: " + line1.getGradient());
     }
 }
