031afba14300aad9c528cdc25e20f15fac20977b
itstarfish <zilvinas@itstarfish.com>
2025-01-24 15:38:13 +0200

    Clear all and 1

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld
+{
+    public static void main (String[] arg){
+
+        int[] numbers = new int[10];
+        numbers[0] = 5;
+        numbers[1] = 8;
+        System.out.println(numbers[0]);
+        System.out.println(numbers[2]);
+        
+
+    }
+}
+
+
+
+
