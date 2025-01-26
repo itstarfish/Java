a55f012bc6f8f04b3a69db643ec840c6654a7cff
itstarfish <zilvinas@itstarfish.com>
2025-01-26 17:25:27 +0200

    Just started

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
 
+        palidromeChecker("feteetef");
+    }
+
+    public static boolean palidromeChecker(String toCheck){
+        LinkedList<Character> stack = new LinkedList<>();
+        toCheck = toCheck.toLowerCase();
+        for(int i = 0; i< toCheck.length();i++){
+            stack.push(toCheck.charAt(i));
+        }
+        for(int i = 0; i< toCheck.length();i++){
+            if (stack.size()> 0 && stack.peek() == toCheck.charAt(i)){
+                System.out.println("toCheck: " + toCheck.charAt(i) + " Stack: " + stack.peek());
+                stack.pop();
+                stack.remove(0);
+
+            } else return false;
+        }
+        return true;
     }
 }
